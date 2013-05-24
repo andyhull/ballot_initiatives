28d3fec57c5e1596409122bfe4adf6e2d609fe09
Andy Hull <andyhull3@gmail.com>
2013-05-24 10:50:06 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BallotInitiatives::Application.config.secret_token = '28f47c1dc369f3a6987984e75b85180231e23c79de9c99ca9e2b48c2a109f814a1366b101939216d1df1b2c4636adecc3aa3430019fc214e2f1054f5c5824f34'
